a1962fe0fcc6ddc145ae62812e62d1db2b6febf8
bluffity <69271308+bluffity@users.noreply.github.com>
2020-08-05 18:19:39 -0500

    Update and rename index.js to main.js

---
+++ public/main.js
@@ -0,0 +1,19 @@
+console.log('Hello world from client-side JS!')
+
+const item = document.querySelector('.item');
+const img = item.querySelector('img');
+const title = item.querySelector('h3');
+const desc = item.querySelector('p');
+
+const renderIt = (data) => {
+  let rec = data.records[0].fields;
+  title.innerHTML = rec.Company;
+  img.src = rec.Logo[0].url;
+  desc.innerHTML = rec.Type[0];
+  
+//   console.log(rec);
+};
+
+fetch(`https://api.airtable.com/v0/${process.env.AT_APP_ID}/Programs?api_key=${process.env.AT_API_KEY}`)
+  .then(response => response.json())
+  .then(data => renderIt(data));
